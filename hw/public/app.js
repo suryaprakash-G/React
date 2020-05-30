52ad5ff19be1326d8c9e342d4bd8549531946e8f
surya <suryagopinathan@gmail.com>
2020-05-30 19:59:56 +0530

    hello world app on react

---
+++ hw/public/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var tem = React.createElement(
+  "p",
+  null,
+  "cool !"
+);
+var approot = document.getElementById("app");
+ReactDOM.render(tem, approot);
---
+++ hw/src/app.js
@@ -0,0 +1,3 @@
+var tem = <p>cool !</p>;
+var approot=document.getElementById("app");
+ReactDOM.render(tem,approot)
