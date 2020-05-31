6146ebf2fac0aec23eaf62eb86f7c4c926ab9a96
surya <suryagopinathan@gmail.com>
2020-05-31 22:41:30 +0530

    practically nothing changed but learnt html and css lot today

--- hw/public/app.js
+++ hw/public/app.js
@@ -1,9 +1,27 @@
 "use strict";
 
 var tem = React.createElement(
-  "p",
-  null,
-  "cool !"
+  "div",
+  { id: "surya" },
+  React.createElement(
+    "h1",
+    null,
+    "surya !"
+  ),
+  React.createElement(
+    "ul",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "22"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "trichy"
+    )
+  )
 );
 var approot = document.getElementById("app");
 ReactDOM.render(tem, approot);
--- hw/src/app.js
+++ hw/src/app.js
@@ -1,3 +1,8 @@
-var tem = <p>cool !</p>;
+var tem =( <div id="surya">
+<h1>surya !</h1>
+<ul>
+<li>22</li><li>trichy</li>
+</ul>
+</div>);
 var approot=document.getElementById("app");
 ReactDOM.render(tem,approot)
