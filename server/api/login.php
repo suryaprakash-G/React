d17fd06689fdf1258184317491b22aaacb09ca70
surya <suryagopinathan@gmail.com>
2020-06-15 21:09:02 +0530

    starting with cookie storage

--- server/api/login.php
+++ server/api/login.php
@@ -1,5 +1,17 @@
 <?php
-$cookie_name = "user";
-$cookie_value = "John Doe";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+$usr=$_POST['usr'];//user name
+$pas=$_POST['pas'];//password
+$db_pass_admin='adminpassword';
+$db_pass_delivery='deliverypassword';
+if(($usr=='admin')&&($pas==$db_pass_admin)){
+    $cookie_name = "shop";
+    $cookie_value = "admin";
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+}elseif(($usr=='delivery')&&($pas==$db_pass_delivery)){
+    $cookie_name = "shop";
+    $cookie_value = "deliver";
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+}else{
+    echo "cr";//credeltial error
+}
 ?>
