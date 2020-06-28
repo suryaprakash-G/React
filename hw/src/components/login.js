a2bcf13eb8932c9aad1d9898ab2adb3eea2f5c25
surya <suryagopinathan@gmail.com>
2020-06-28 14:50:24 +0530

    started axios

--- hw/src/components/login.js
+++ hw/src/components/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import '../style/login.css';
 //import { CookiesProvider } from 'react-cookie';
 //const adminpass="surya";
--- server/api/login.php
+++ server/api/login.php
@@ -4,13 +4,15 @@ $pas=$_POST['pas'];//password
 $db_pass_admin='adminpassword';
 $db_pass_delivery='deliverypassword';
 if(($usr=='admin')&&($pas==$db_pass_admin)){
-    $cookie_name = "shop";
-    $cookie_value = "admin";
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+    #$cookie_name = "shop";
+    #$cookie_value = "admin";
+    #setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+	echo "lg";
 }elseif(($usr=='delivery')&&($pas==$db_pass_delivery)){
-    $cookie_name = "shop";
-    $cookie_value = "deliver";
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+    #$cookie_name = "shop";
+    #$cookie_value = "deliver";
+    #setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+	echo "lg";
 }else{
     echo "cr";//credeltial error
 }
