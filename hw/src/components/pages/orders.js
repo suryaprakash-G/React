68284cb51d48fcc038a0c2e276d9d3573e5ecc4d
surya <suryagopinathan@gmail.com>
2020-07-12 22:27:04 +0530

    one line at a time lol

--- hw/src/components/pages/orders.js
+++ hw/src/components/pages/orders.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Sidenav from '../sidenav';
 class Orders extends React.Component{
     render(){
         return(
             <div className="del">
             <div className="side nav"></div>
-            <div className="side nav"></div>
+            <div className="main"></div>
             </div>
         )
     }
